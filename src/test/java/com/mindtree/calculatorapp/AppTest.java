82885e60ca82ab6f0a47c83c3921dfcb15896d21
sujjadshaik <sujjamohammedshaik@gmail.com>
2021-02-23 14:54:21 +0530

    updated jenkinfile

--- src/test/java/com/mindtree/calculatorapp/AppTest.java
+++ src/test/java/com/mindtree/calculatorapp/AppTest.java
@@ -13,11 +13,8 @@ public class AppTest
 {
 	@Test
 	public void testSum() {
-		// Given
 		App calculator = new App();
-		// When
 		int result = calculator.sum(2, 2);
-		// Then
 		if (result != 4) {   // if 2 + 2 != 4
 			Assert.fail();
 		}
